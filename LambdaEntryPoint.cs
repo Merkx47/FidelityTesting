650b10a603bcbbeec98f90a31851b3eed189cd9c
emeka <emeka.udemezue@qucoon.com>
2025-07-03 10:44:47 +0100

    LambdaEntryPoint.cs & Program.cs file update

--- LambdaEntryPoint.cs
+++ LambdaEntryPoint.cs
@@ -1,3 +1,4 @@
+// LambdaEntryPoint.cs (ONLY file we needed to add)
 using Amazon.Lambda.AspNetCoreServer;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
@@ -12,29 +13,80 @@ public class LambdaEntryPoint : APIGatewayProxyFunction
     {
         builder
             .UseContentRoot(Directory.GetCurrentDirectory())
-            .UseStartup<Startup>()
+            .ConfigureServices(services =>
+            {
+                // Mirror your original Program.cs services
+                services.AddControllers();
+                services.AddEndpointsApiExplorer();
+                services.AddSwaggerGen();
+            })
+            .Configure(app =>
+            {
+                // Mirror your original Program.cs pipeline
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseRouting();
+                app.UseAuthorization();
+                app.UseEndpoints(endpoints => endpoints.MapControllers());
+            })
             .UseLambdaServer();
     }
 }
 
-public class Startup
-{
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddControllers();
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-    }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseRouting();
-        app.UseAuthorization();
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using Amazon.Lambda.AspNetCoreServer;
+// using Amazon.Lambda.Core;
+// using Amazon.Lambda.Serialization.SystemTextJson;
+
+// [assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+
+// namespace FidelityTesting;
+
+// public class LambdaEntryPoint : APIGatewayProxyFunction
+// {
+//     protected override void Init(IWebHostBuilder builder)
+//     {
+//         builder
+//             .UseContentRoot(Directory.GetCurrentDirectory())
+//             .UseStartup<Startup>()
+//             .UseLambdaServer();
+//     }
+// }
+
+// public class Startup
+// {
+//     public void ConfigureServices(IServiceCollection services)
+//     {
+//         services.AddControllers();
+//         services.AddEndpointsApiExplorer();
+//         services.AddSwaggerGen();
+//     }
+
+//     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//     {
+//         app.UseSwagger();
+//         app.UseSwaggerUI();
+//         app.UseRouting();
+//         app.UseAuthorization();
+//         app.UseEndpoints(endpoints =>
+//         {
+//             endpoints.MapControllers();
+//         });
+//     }
+// }
